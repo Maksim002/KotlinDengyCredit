a2721290aa1e2fa13c1d0a1aacf97239b8f6870e
locad <inoplanee@mail.ru>
2020-10-14 13:57:51 +0600

    14.10.2020(13.57)

---
+++ app/src/main/java/com/example/kotlincashloan/ui/notification/FirebaseMessagingService.java
@@ -0,0 +1,84 @@
+package com.example.kotlincashloan.ui.notification;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.kotlincashloan.R;
+import com.example.kotlincashloan.ui.registration.login.HomeActivity;
+import com.example.kotlinscreenscanner.ui.MainActivity;
+import com.google.firebase.messaging.RemoteMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    private static final String TAG = "FirebaseMessagingServic";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Check if message contains a data payload.
+        remoteMessage.getData();
+
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            try {
+                JSONObject data = new JSONObject(remoteMessage.getData());
+                String jsonMessage = data.getString("extra_information");
+                Log.d(TAG, "onMessageReceived: \n" +
+                        "Extra Information: " + jsonMessage);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            String title = remoteMessage.getNotification().getTitle(); //get title
+            String message = remoteMessage.getNotification().getBody(); //get message
+            String click_action = remoteMessage.getNotification().getClickAction(); //get click_action
+
+            Log.d(TAG, "Message Notification Title: " + title);
+            Log.d(TAG, "Message Notification Body: " + message);
+            Log.d(TAG, "Message Notification click_action: " + click_action);
+
+            sendNotification(title, message, click_action);
+        }
+    }
+
+    @Override
+    public void onDeletedMessages() {
+
+    }
+
+    private void sendNotification(String title, String messageBody, String click_action) {
+        Intent intent = null;
+        if (click_action.equals("SOMEACTIVITY")) {
+            intent = new Intent(this, HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setContentText(messageBody)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
--- app/src/main/java/com/example/kotlincashloan/ui/notification/PushNotification.kt
+++ app/src/main/java/com/example/kotlincashloan/ui/notification/PushNotification.kt
@@ -16,18 +16,15 @@ import com.google.firebase.messaging.RemoteMessage
 class PushNotification : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
-        showNotification(p0.notification?.title.toString(), p0.notification?.body.toString())
-
+        showNotification(p0.notification?.title.toString(), p0.notification?.body.toString(), p0.notification?.clickAction.toString())
     }
 
-    private fun showNotification(title: String, message: String) {
+    private fun showNotification(title: String, message: String, action: String) {
 
         FirebaseMessaging.getInstance().subscribeToTopic("kotlincashloan")
 
         val notChannelId = "com.example.kotlincashloan"
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val c = NotificationChannel(notChannelId, "kotlincashloan", NotificationManager.IMPORTANCE_DEFAULT)
@@ -35,7 +32,6 @@ class PushNotification : FirebaseMessagingService() {
             c.enableLights(true)
             c.lightColor = Color.BLUE
             notificationManager.createNotificationChannel(c)
-
         }
 
         val builder = NotificationCompat.Builder(this, notChannelId)
