3d6fa0b06f621816151c2c430b9267cad100fe07
locad <inoplanee@mail.ru>
2020-11-11 14:05:43 +0600

    11.11.2020(14:05)

--- app/src/main/java/com/example/kotlincashloan/ui/loans/fragment/LoanStepTwoFragment.kt
+++ app/src/main/java/com/example/kotlincashloan/ui/loans/fragment/LoanStepTwoFragment.kt
@@ -1,17 +1,24 @@
 package com.example.kotlincashloan.ui.loans.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
 import com.example.kotlincashloan.R
 import com.example.kotlinscreenscanner.ui.MainActivity
 import kotlinx.android.synthetic.main.fragment_loan_step_two.*
 
 class LoanStepTwoFragment : Fragment() {
+    private val sumMax = 25
+    private val sumMin = 0
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_loan_step_two, container, false)
@@ -20,14 +27,23 @@ class LoanStepTwoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initSeekBar()
+        initImageView()
     }
 
+    @SuppressLint("NewApi")
     private fun initSeekBar() {
+        val max = sumMax - 5
+        val min = sumMin
+        loan_step_seek.max = max
+        loan_step_seek.min = min
         loan_step_seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                val seekbarValue = i.toString()
 
-                loan_step_sum.setText(seekbarValue)
+                seekBar.min = sumMin
+                val seekbarValue = i.toString()
+                var resultSum = 0
+                resultSum = seekbarValue.toInt() * 1000 + 5000
+                progressBarr(resultSum.toString())
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
@@ -35,14 +51,47 @@ class LoanStepTwoFragment : Fragment() {
         })
     }
 
-    fun progressBarr(i: Int){
-        var seck = i
-        var num = 25000
+    fun progressBarr(i: String){
+        loan_step_sum.setText(i)
 
+    }
+
+    private fun initImageView() {
+        val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+        params.weight = 1.0f
+        params.gravity = Gravity.CENTER
+        val sum = sumMax - sumMin
+        val pairs = arrayOfNulls<ImageView>(sum)
+        var v = pairs.size -1
+
+        for (l in 0..v) {
+            val p = pairs.size / 2
+            if (l == 0){
+                nestedImage(l, pairs, params)
+                pairs[l]!!.setImageDrawable(resources.getDrawable(R.drawable.ic_coordinate_maxi))
+            }else if (l == v){
+                nestedImage(l, pairs, params)
+                pairs[l]!!.setImageDrawable(resources.getDrawable(R.drawable.ic_coordinate_maxi))
+            }else if (l == p){
+                nestedImage(l, pairs, params)
+                pairs[l]!!.setImageDrawable(resources.getDrawable(R.drawable.ic_coordinate_maxi))
+            }else{
+                nestedImage(l, pairs, params)
+                pairs[l]!!.setImageDrawable(resources.getDrawable(R.drawable.ic_coordinate_mini))
+            }
+            loan_layout.addView(pairs[l])
+        }
+    }
+    private fun nestedImage(number: Int, pairs: Array<ImageView?> = arrayOfNulls(0), params: LinearLayout.LayoutParams) {
+        pairs[number] = ImageView(context)
+        pairs[number]!!.layoutParams = params
+        pairs[number]!!.id = number
+        pairs[number]!!.setPadding(10, 5, 10, 5)
     }
 
     override fun onResume() {
         super.onResume()
         MainActivity.timer.timeStop()
+        loan_step_sum.text = "5000"
     }
 }
