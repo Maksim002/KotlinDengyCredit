7b9e185584765796013a1f78739271d44c21917c
Stap <inoplanee@mail.ru>
2021-03-04 16:49:16 +0600

    04.03.21(16.48)Сделать дизайн виджета выбора даты

--- app/src/main/java/com/example/kotlincashloan/ui/loans/GetLoanActivity.kt
+++ app/src/main/java/com/example/kotlincashloan/ui/loans/GetLoanActivity.kt
@@ -29,7 +29,6 @@ class GetLoanActivity : AppCompatActivity() {
         lateinit var alert: LoadingAlert
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_loan)
